ef7c3dc9d3d4664610f8ed7a27da19523935eb48
Batu <batuyunculer@gmail.com>
2021-05-06 14:53:29 +0300

    google maps eklendi

---
+++ Components/map.js
@@ -0,0 +1,5 @@
+function Map() {
+  return <iframe className="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3259.6583562367505!2d33.34655031524607!3d35.214977980305754!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14de16d2ad6d0939%3A0x13e544d93b02b855!2zw4dlbGVyIEFoxZ9hcCBLYXDEsQ!5e0!3m2!1str!2str!4v1620301300598!5m2!1str!2str" allowFullScreen="" loading="lazy"></iframe>;
+}
+
+export default Map;
